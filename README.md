8cafa5cc1b414f204e283b3a1da8c44e8311be0b
colewortman <wortcole12@gmail.com>
2025-03-17 17:06:24 -0500

    Read and write files

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-This program allows users to choose one of the following sorting methods and input a file with numbers to sort: bubble, insertion, merge, quick, selection.
-This program will write to the given output file with the sorted data and how much time it took to sort.
+This program allows users to choose one of the following sorting methods: bubble, insertion, merge, quick, selection.
+Input files are txt files with numbers separated by commas.
+This program will write to the given output file with the sorted numbers and how much time it took to sort.
 This program is run on the command line with the usage: .\sort_machine.py [sorting method] [input_file] [output_file]
--- sort_machine.py
+++ sort_machine.py
@@ -2,50 +2,61 @@ import sys
 
 methods = ["bubble", "insertion", "merge", "quick", "selection"]
 
-def bubble_sort(f):
+def bubble_sort(nums):
 
-    print("bubbles!")
+    return nums
 
-def insertion_sort(f):
+def insertion_sort(nums):
 
-    print("insert")
+    return nums
 
-def merge_sort(f):
+def merge_sort(nums):
     
-    print("merge")
+    return nums
 
-def quick_sort(f):
+def quick_sort(nums):
 
-    print("quick")
+    return nums
 
-def selection_sort(f):
+def selection_sort(nums):
     
-    print("select")
+    return nums
 
+def write_data(data, outf):
+
+    with open(outf, "w") as f:
+        f.write(", ".join(map(str, data)))
 
 def main():
 
-    if len(sys.argv) != 3 or sys.argv[1] not in methods:
-        print("\nUsage: sort_machine.py [sorting method] [file]")
+    if len(sys.argv) != 4 or sys.argv[1] not in methods:
+        print("\nUsage: sort_machine.py [sorting method] [input_file] [output_file]")
         print(f"Sorting methods: {", ".join(methods)}\n")
         return
     
     input_method = sys.argv[1]
     input_file = sys.argv[2]
+    output_file = sys.argv[3]
+
+    with open(input_file, "r") as f:
+        data = list(map(int, f.read().split(",")))
     
     match input_method:
         case "bubble":
-            bubble_sort(input_file)
+            sorted_data = bubble_sort(data)
         case "insertion":
-            insertion_sort(input_file)
+            sorted_data = insertion_sort(data)
         case "merge":
-            merge_sort(input_file)
+            sorted_data = merge_sort(data)
         case "quick":
-            quick_sort(input_file)
+            sorted_data = quick_sort(data)
         case "selection":
-            selection_sort(input_file)
+            sorted_data = selection_sort(data)
         case _:
             print("Invalid sorting method")
+            return
+    
+    write_data(sorted_data, output_file)
 
 if __name__ == "__main__":
     main()
